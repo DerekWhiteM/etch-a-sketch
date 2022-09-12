9238d49f438c1302c594b5acfd1cc91de90b3d74
Derek White <derekwhite.m@gmail.com>
2022-09-11 20:36:22 -0400

    add hover effect so divs change color

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ function createGrid() {
     for (let i = 0; i < (256); i++) {
         const div = document.createElement('div');
         div.classList.add('item');
+        div.addEventListener('mouseenter', handleMouseEnter);
         container.appendChild(div);
     }
 
     body.appendChild(container);
+
+    function handleMouseEnter(e) {
+        e.currentTarget.classList.add('marked');
+    }
 
 }
 
